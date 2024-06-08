9a412160ec544c29a10346121bb90d256bff9931
Richa Patil <54523621+richapatil@users.noreply.github.com>
2024-06-08 16:13:42 +0530

    Create Solution.java

---
+++ 1295. Find Numbers with Even Number of Digits/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (String.valueOf(nums[i]).length() % 2 == 0) {
+
+                count++;
+
+            }
+        }
+
+        return count;
+
+    }
+}
