b8c4f3a9f623339656aa70297428f53940d3867e
Richa Patil <54523621+richapatil@users.noreply.github.com>
2024-06-17 01:35:15 +0530

    Update Solution.java

--- 414. Third Maximum Number/Solution.java
+++ 414. Third Maximum Number/Solution.java
@@ -1,1 +1,23 @@
+class Solution {
+  
+    public int thirdMax(int[] nums) {
+        
+        Set<Integer> newList = new HashSet<>();
+        
+        for (int val : nums) {
+            newList.add(val);
+        }
 
+        List<Integer> sortedNumbers = new ArrayList<>(newList);
+        Collections.sort(sortedNumbers);
+
+        if(sortedNumbers.size() < 3){
+
+            return sortedNumbers.getLast();
+
+        }else{
+
+            return sortedNumbers.get(sortedNumbers.size()-3);
+        }
+    }
+}
