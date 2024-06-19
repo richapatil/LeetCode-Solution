d0a25dc22ffaf47c3f92b99b05111ed736a43361
Richa Patil <54523621+richapatil@users.noreply.github.com>
2024-06-20 00:11:20 +0530

    Create Solution.java

---
+++ 1342. Number of Steps to Reduce a Number to Zero/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+  
+    public int numberOfSteps(int num) {
+
+        int stepsCount = 0;
+        
+        while (num > 0){
+
+            if(num % 2 == 0){
+                
+                num = num/2;
+                
+            }else{
+                num = num -1;
+                
+            }
+
+            stepsCount++;
+            
+        }
+
+        return stepsCount;
+
+    }
+}
