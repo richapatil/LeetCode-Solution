6d40e3e416c17d8c17e56e2a8764f0237d6c0df0
Richa Patil <54523621+richapatil@users.noreply.github.com>
2024-06-26 00:19:32 +0530

    Update Solution.java

--- 747. Largest Number At Least Twice of Others/Solution.java
+++ 747. Largest Number At Least Twice of Others/Solution.java
@@ -1,1 +1,33 @@
+class Solution {
+  
+    public int dominantIndex(int[] nums) {
+        
+        int max = 0;
+        int index = -1;
+        
+        for(int i = 0; i < nums.length; i++){
+            
+            if(nums[i] > max){
+                max =+ nums[i];
+                index = i;
+            }
+        }
+        int middleRange = max/2;
 
+        for(int j = 0; j < nums.length; j++){
+            
+            if(nums[j]==max)
+            {
+                continue;
+            }
+            if(nums[j]>middleRange){
+
+              return -1;
+
+             }
+        }
+
+        return index;
+        
+    }
+}
