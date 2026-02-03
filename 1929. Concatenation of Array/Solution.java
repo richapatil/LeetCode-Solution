829b049e1efa287f2c6042f0fdecef25d5ea76c7
Richa Patil <54523621+richapatil@users.noreply.github.com>
2026-02-03 23:10:25 +0530

    1929. Concatenation of Array

---
+++ 1929. Concatenation of Array/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+
+        int[] ans = Arrays.copyOf(nums, nums.length * 2);
+        int numsLen = nums.length;
+
+        for (int i = 0; i < (nums.length); i++) {
+
+            ans[numsLen] = nums[i];
+            numsLen++;
+
+        }
+
+        return ans;
+    }
+}
+
+//TC - O(n)
+//SC - O(1)
